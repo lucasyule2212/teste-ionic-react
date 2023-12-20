9a35ad4a8c5b57a6f8849649e95ddd7c867f394e
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-20 02:01:59 -0300

    refactor(home): adding GenresList component to Home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,14 @@
 import Carousel from 'components/carousel/carousel'
+import GenresList from 'components/genresList/genresList'
 import useFeaturedMovies from 'hooks/useFeaturedMovies'
 
 export default function Home() {
   const { data, isLoading } = useFeaturedMovies()
-  console.log(data)
 
   return (
     <main className="flex min-h-[90vh] w-full flex-col gap-8 bg-white p-8 dark:bg-zinc-900">
       <Carousel items={data} isLoading={isLoading} />
+      <GenresList />
     </main>
   )
 }
