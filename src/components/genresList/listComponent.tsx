bf965dedc900df5715291afbc9c07c1bb700d8e5
Lucas Yule <lyrma@cin.ufpe.br>
2023-12-20 10:19:11 -0300

    fix(listComponent): removing conditional render at listComponent

--- src/components/genresList/listComponent.tsx
+++ src/components/genresList/listComponent.tsx
@@ -1,5 +1,5 @@
 import useMoviesByGenre from 'hooks/useMoviesByGenre'
-import { block, For } from 'million/react'
+import { block } from 'million/react'
 import MoviePosterBlock from './moviePoster'
 
 const ListComponentBlock = block(
@@ -15,28 +15,27 @@ const ListComponentBlock = block(
       genreId: genre.id
     })
 
+    if (isLoading) {
+      return (
+        <div key={genre.id} className="flex max-w-[90vw] flex-col">
+          <h1 className="ml-2 text-3xl font-semibold text-zinc-700 underline decoration-ionic decoration-solid dark:text-white">
+            {genre.name}
+          </h1>
+          {Array.from(Array(5).keys()).map((_, index) => (
+            <div key={index} className="skeleton h-60 w-44"></div>
+          ))}
+        </div>
+      )
+    }
+
     return (
       <div key={genre.id} className="flex max-w-[90vw] flex-col">
         <h1 className="ml-2 text-3xl font-semibold text-zinc-700 underline decoration-ionic decoration-solid dark:text-white">
           {genre.name}
         </h1>
         <div className="carousel carousel-center mt-2 w-full gap-4 space-x-4">
-          {isLoading ? (
-            Array.from(Array(5).keys()).map((_, index) => (
-              <div key={index} className="skeleton h-60 w-44"></div>
-            ))
-          ) : (
-            <For each={data} memo>
-              {({
-                poster_path,
-                id
-              }: {
-                poster_path: string
-                index: number
-                id: number
-              }) => <MoviePosterBlock key={id} poster_path={poster_path} />}
-            </For>
-          )}
+          {data &&
+            data.map((movie) => <MoviePosterBlock key={movie.id} {...movie} />)}
         </div>
       </div>
     )
